954067e7dd6f901547381658888d87dd39db211d
sauravkumar405 <49597067+sauravkumar405@users.noreply.github.com>
2024-12-04 00:10:51 +0530

    Create debounce.js

---
+++ debounce.js
@@ -0,0 +1,9 @@
+function debounce(func, delay){
+    let timeout
+    return function(...args){
+        clearTimeout(timeout)
+        timeout = setTimeout(()=>{
+            func(...args)
+        }, delay)
+    }
+}
