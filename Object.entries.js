068e669142c8e7f562f0add5e4d32b6afef22064
sauravkumar405 <49597067+sauravkumar405@users.noreply.github.com>
2024-12-04 14:15:27 +0530

    Create Object.entries.js

---
+++ Object.entries.js
@@ -0,0 +1,10 @@
+Object.prototype.myEntries = function(obj){
+    let result = []
+    for(let key of Object.keys(obj)){
+        result.push([key, obj[key]])
+    }
+    return result
+}
+
+const obj = { a: 1, b: 2, c: 3 };
+console.log(Object.myEntries(obj));
